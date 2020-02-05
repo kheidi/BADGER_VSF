6fe166feec6a639f1d6eb3f7c07b808e71bc3e95
Katherine Fehr <kfehr@wisc.edu>
2020-02-05 17:16:52 -0600

    Adds perpendicular distance from shank segment

--- XSENSMarkers.m
+++ XSENSMarkers.m
@@ -23,9 +23,13 @@ RSK4_all = RSK4{1,1}(:,1:3);
 
 %% Loop
 
-frames = [458 570 580 560 627 640 750 760 740 1187 1243 822 1400];
+%Randomly generated frames from 410 - 870
+frames = randi([410 870], 1, 200);
 diff = zeros(length(frames),1);
 diffR = zeros(length(frames),1);
+iP_normaldiv = zeros(length(frames),3);
+store= zeros(length(frames),3);
+
 for i = 1: length(frames)
     frame_curr = frames(i);
     sAngle = shankAngle_all(frame_curr);
@@ -34,45 +38,25 @@ for i = 1: length(frames)
     iP4 = RSK4_all(frame_curr, :);
     iP = [iP2;iP3;iP4];
     iP_normal = planeNormal(iP3,iP4,iP2);
-    iP_normaldiv = iP_normal/iP_normal(2);
+    store(i,:) = iP_normal;
+    %Normalizes
+    iP_normaldiv(i,:) = iP_normal/iP_normal(2);
     iP_angD = atand(iP_normal(2)/iP_normal(3));
     iP_angR = atan(iP_normal(2)/iP_normal(3));
     diff(i) = iP_angD + sAngle;
     diffR(i) = diff(i) * (pi/180);
 end
 
-diffR
+diffR;
 diffAvg = mean(diffR)
 diffS = std(diffR)
-    
-
-%% vsfxa-004_ConditionB_Setting3_Take2.c3d
-%frame 276
-% shankAngle = 17.0191;
-% RSK3 = [-0.155091 0.159179 0.321497];
-% RSK4 = [-0.153173 0.102854 0.300557];
-% RSK2 = [-0.237762 0.126015 0.305196];
-% iP = [RSK3;RSK4;RSK2];
-% iP_normal = planeNormal(RSK3,RSK4,RSK2)
-% iP_normaldiv = iP_normal/iP_normal(2)
-% iP_angD = atand(iP_normal(2)/iP_normal(3))
-% iP_angR = atan(iP_normal(2)/iP_normal(3))
-% diff = iP_angD + shankAngle
-% diffR = diff * (pi/180)
-
-%% vsfxa-004_ConditionA_Setting3_Take2.c3d
-%frame 171
-% shankAngle = [-17.6959552764893];
-% RSK3 = [-0.421580 0.219827 0.311092];
-% RSK4 = [-0.510214 0.223965 0.321821];
-% RSK2 = [-0.435778 0.271934 0.330057];
-% iP = [RSK3;RSK4;RSK2];
-% iP_normal = planeNormal(RSK3,RSK4,RSK2);
-% iP_normaldiv = iP_normal/iP_normal(3);
-% iP_angD = atand(iP_normal(2)/iP_normal(3));
-% iP_angR = atan(iP_normal(2)/iP_normal(3));
-% diff = iP_angD - shankAngle
-% diffR = diff * (pi/180)
+figure
+plot(frames, diffR, '*' )
+lsline
+figure
+bar(abs(diffAvg))
+hold on
+errorbar(abs(diffAvg), abs(diffS))
 
 %% Graph math
 % Z = iP_normaldiv(2)/tand(sAngle);
@@ -86,3 +70,56 @@ diffS = std(diffR)
 % ylim([-(maxVal+10) maxVal+10]);
 % xline(0);
 % yline(0);
+% legend('iPecs Normal', 'Shank Angle')
+
+%% Translation
+rInstance = frames(1)
+rKnee = RKNEE{1,1}(:,1:3);
+vsf_ankle = VSF_RANKLE{1,1}(:,1:3);
+
+
+%Midpoint between RSK3 and RSK4
+dx = (RSK3_all(rInstance, 1)+(RSK4_all(rInstance, 1)))/2;
+dy = (RSK3_all(rInstance, 2)+(RSK4_all(rInstance, 2)))/2;
+dz = (RSK3_all(rInstance, 3)+(RSK4_all(rInstance, 3)))/2;
+mp1 = [dx, dy, dz]
+%Mid point between mp1 and rsk2
+dx = (mp1(1)+(RSK2_all(rInstance, 1)))/2;
+dy = (mp1(2)+(RSK2_all(rInstance, 2)))/2;
+dz = (mp1(3)+(RSK2_all(rInstance, 3)))/2;
+mp2 = [dx, dy, dz]
+iPMid = mp2;
+
+rKnee = rKnee(rInstance,:);
+vsf_ankle = vsf_ankle(rInstance,:);
+
+iP2K_m =(rKnee(2)-iPMid(2))/(rKnee(3)-iPMid(3))
+K2A_m = (rKnee(2)-vsf_ankle(2))/(rKnee(3)-vsf_ankle(3))
+%angle between shanklength and knee to point
+theta = atand(abs((K2A_m-iP2K_m)/(1+K2A_m*iP2K_m)))
+
+
+%Perpendicular distance from shank
+%distance from iPecs center to knee
+iP2K = sqrt((rKnee(2)-iPMid(2))^2+(rKnee(3)-iPMid(3))^2)
+K2A = sqrt((rKnee(2)-vsf_ankle(2))^2+(rKnee(3)-vsf_ankle(3))^2)
+%angle between shanklength and knee to point
+%distance from midpoint to shank midline
+dist = iP2K*sind(theta)
+
+hold off
+figure
+plot3(RSK2_all(rInstance,1), RSK2_all(rInstance,2), RSK2_all(rInstance,3), 'o')
+hold on
+plot3(RSK3_all(rInstance,1), RSK3_all(rInstance,2), RSK3_all(rInstance,3), 'o')
+plot3(RSK4_all(rInstance,1), RSK4_all(rInstance,2), RSK4_all(rInstance,3), 'o')
+plot3([rKnee(1,1) vsf_ankle(1,1)],[rKnee(1,2) vsf_ankle(1,2)], [rKnee(1,3) vsf_ankle(1,3)])
+plot3(iPMid(1,1), iPMid(1,2), iPMid(1,3), '*')
+grid on
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
+hold off
+
+
+
