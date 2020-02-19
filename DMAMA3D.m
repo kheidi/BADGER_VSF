c9d28f99740b0e5b4542f52642d38876bd957dc0
Katherine Fehr <kfehr@wisc.edu>
2020-02-18 18:26:09 -0600

    Completed DMAMA3D code

--- DMAMA3D.m
+++ DMAMA3D.m
@@ -11,7 +11,7 @@
 % Set subject and stiffness setting, this will find correct data in
 %matrices of provided, known values
 subject = 4;
-setting = 1;
+setting = 3;
 cd Data
 
 %Using vectorCalc3D file find the average A matrix and r vector for each
@@ -463,6 +463,13 @@ for i = 1:length(ipFz)
     F_shankframe(i,:) = transpose(A * [ipFx(i); ipFy(i); ipFz(i)]);
 end
 
+% Add moment from iPecs
+
+%Check signs
+F_shankframe(:,1) = F_shankframe(:,1) + MxiPecs(:,1);
+F_shankframe(:,2) = F_shankframe(:,2) + MyiPecs(:,1);
+F_shankframe(:,3) = F_shankframe(:,3) + MziPecs(:,1);
+
 figure
 subplot(2,1,1)
 xlim([0 length(F_shankframe)/4])
@@ -481,11 +488,394 @@ plot(F_shankframe(:,3), '-b')
 legend('Fx Shank Frame', 'Fy Shank Frame','Fz Shank Frame')
 title('Transformed iPecs Forces');
 
+%Average New FX FY and FZ for comparing
+
+Fx_Avg = mean(F_shankframe(:,1));
+Fy_Avg = mean(F_shankframe(:,2));
+Fz_Avg = mean(F_shankframe(:,3));
+
 % Moment r X f
 
+clear moment_all
 for i = 1:length(F_shankframe)
     moment_all(i,:) = cross(r, F_shankframe(i,:));
 end
 
 moment_withXsensTime(:,1) = newTime;
 moment_withXsensTime(:,2:4) = moment_all;
+
+figure
+yyaxis left
+plot(newTime, moment_all(:,1), 'b-')
+ylim([-220,220])
+xlabel('iPecs Time')
+ylabel('Moment')
+yyaxis right
+plot(toeZPos(xsensStart:xsensEnd,1), toeZPos(xsensStart:xsensEnd,4)*250, 'r-')
+ylabel('Toe Position')
+ylim([-1600,1600])
+title('New Moment & Toe Position')
+
+%% Find Amb tasks in iPecs time
+
+%Find function: find(condition, number or results to return, direction it 
+%is searching in)
+
+urStart1 = find(moment_withXsensTime(:,1)>=urStart1X,1,'first');
+urEnd1 = find(moment_withXsensTime(:,1)>=urEnd1X,1,'first');
+lgStart1 = find(moment_withXsensTime(:,1)>=lgStart1X,1,'first');
+lgEnd1 = find(moment_withXsensTime(:,1)>=lgEnd1X,1,'first');
+drStart1 = find(moment_withXsensTime(:,1)>=drStart1X,1,'first');
+drEnd1 = find(moment_withXsensTime(:,1)>=drEnd1X,1,'first');
+urStart2 = find(moment_withXsensTime(:,1)>=urStart2X,1,'first');
+urEnd2 = find(moment_withXsensTime(:,1)>=urEnd2X,1,'first');
+lgStart2 = find(moment_withXsensTime(:,1)>=lgStart2X,1,'first');
+lgEnd2 = find(moment_withXsensTime(:,1)>=lgEnd2X,1,'first');
+usStart1 = find(moment_withXsensTime(:,1)>=usStart1X,1,'first');
+usEnd1 = find(moment_withXsensTime(:,1)>=usEnd1X,1,'first');
+usStart2 = find(moment_withXsensTime(:,1)>=usStart2X,1,'first');
+usEnd2 = find(moment_withXsensTime(:,1)>=usEnd2X,1,'first');
+dsStart1 = find(moment_withXsensTime(:,1)>=dsStart1X,1,'first');
+dsEnd1 = find(moment_withXsensTime(:,1)>=dsEnd1X,1,'first');
+dsStart2 = find(moment_withXsensTime(:,1)>=dsStart2X,1,'first');
+dsEnd2 = find(moment_withXsensTime(:,1)>=dsEnd2X,1,'first');
+lgStart3 = find(moment_withXsensTime(:,1)>=lgStart3X,1,'first');
+lgEnd3 = find(moment_withXsensTime(:,1)>=lgEnd3X,1,'first');
+drStart2 = find(moment_withXsensTime(:,1)>=drStart2X,1,'first');
+drEnd2 = find(moment_withXsensTime(:,1)>=drEnd2X,1,'first');
+
+% Figure 4: Graph Z Ground Reaction Force, HC & TO
+titleV=(['Moment with Amb Tasks: Subject ', num2str(subject), ' on Setting ', num2str(setting)]);
+figure
+subplot(2,1,1)
+hold on
+plot(moment_all(:,1), 'k-')
+xline(urStart1, ':b', 'UR1 Start','HandleVisibility','off');
+xline(urEnd1, ':r', 'UR1 End','HandleVisibility','off');
+xline(lgStart1, ':b', 'LG1 Start','HandleVisibility','off');
+xline(lgEnd1, ':r', 'LG1 End','HandleVisibility','off');
+xline(drStart1, ':b', 'DR1 Start','HandleVisibility','off');
+xline(urStart2, ':b', 'UR2 Start','HandleVisibility','off');
+xline(lgStart2, ':b', 'LG2 Start','HandleVisibility','off');
+xline(usStart1, ':b', 'US1 Start','HandleVisibility','off');
+xline(usStart2, ':b', 'US2 Start','HandleVisibility','off');
+xline(dsStart1, ':b', 'DS1 Start','HandleVisibility','off');
+xline(dsStart2, ':b', 'DS2 Start','HandleVisibility','off');
+xline(lgStart3, ':b', 'LG3 Start','HandleVisibility','off');
+xline(drStart2, ':b', 'DR2 Start','HandleVisibility','off');
+xlim([0 length(moment_all)])
+xlabel('iPecs Frame/Time')
+ylabel('Moment (Nm)')
+title(titleV)
+hold off
+
+titleV=(['Toe Position in Z: Subject ', num2str(subject), ' on Setting ', num2str(setting)]);
+subplot(2,1,2)
+hold on
+plot(toeZPos(xsensStart:xsensEnd,1), toeZPos(xsensStart:xsensEnd,4), 'r-')
+xlim([xsensStart xsensEnd])
+xline(urStart1X, ':b', 'UR1 Start','HandleVisibility','off');
+xline(urEnd1X, ':r', 'UR1 End','HandleVisibility','off');
+xline(lgStart1X, ':b', 'LG1 Start','HandleVisibility','off');
+xline(lgEnd1X, ':r', 'LG1 End','HandleVisibility','off');
+xline(drStart1X, ':b', 'DR1 Start','HandleVisibility','off');
+xline(urStart2X, ':b', 'UR2 Start','HandleVisibility','off');
+xline(lgStart2X, ':b', 'LG2 Start','HandleVisibility','off');
+xline(usStart1X, ':b', 'US1 Start','HandleVisibility','off');
+xline(usStart2X, ':b', 'US2 Start','HandleVisibility','off');
+xline(dsStart1X, ':b', 'DS1 Start','HandleVisibility','off');
+xline(dsStart2X, ':b', 'DS2 Start','HandleVisibility','off');
+xline(lgStart3X, ':b', 'LG3 Start','HandleVisibility','off');
+xline(drStart2X, ':b', 'DR2 Start','HandleVisibility','off');
+xlabel('XSENS Frame/Time')
+ylabel('Height (m)')
+title(titleV)
+
+%% SECTION 9: FIND MEAN OF MOMENT AND SAG FORCE FOR EACH AMB TASK
+%These next sections will be broken up into each amb task
+momentxyz = moment_all;
+moment_all = moment_all(:,1);
+
+figure
+yyaxis left
+plot((-1)*moment_all, '-b')
+ylabel('Moment')
+%ylim([-40 900])
+yyaxis right
+plot(sagForce, '-r')
+ylabel('Force')
+%ylim([-40 900])
+xlim([-1000 length(moment_all)+1000])
+xline(urStart1, ':b', 'UR1 Start','HandleVisibility','off');
+xline(urEnd1, ':r', 'UR1 End','HandleVisibility','off');
+xline(lgStart1, ':b', 'LG1 Start','HandleVisibility','off');
+xline(lgEnd1, ':r', 'LG1 End','HandleVisibility','off');
+xline(drStart1, ':b', 'DR1 Start','HandleVisibility','off');
+xline(urStart2, ':b', 'UR2 Start','HandleVisibility','off');
+xline(lgStart2, ':b', 'LG2 Start','HandleVisibility','off');
+xline(usStart1, ':b', 'US1 Start','HandleVisibility','off');
+xline(usStart2, ':b', 'US2 Start','HandleVisibility','off');
+xline(dsStart1, ':b', 'DS1 Start','HandleVisibility','off');
+xline(dsStart2, ':b', 'DS2 Start','HandleVisibility','off');
+xline(lgStart3, ':b', 'LG3 Start','HandleVisibility','off');
+xline(drStart2, ':b', 'DR2 Start','HandleVisibility','off');
+
+%% Wrong direction of moment?
+moment_all = -1*(moment_all);
+%% SECTION 9A: UP RAMP ANALYSIS
+count = 0;
+sumForce = 0;
+sumMoment = 0;
+for i = 1:length(moment_all)
+    if i >= urStart1 && i < urEnd1 + 1 || ...
+            i >= urStart2 && i < urEnd2 + 1
+        count = count + 1;
+        urForce(count,1) = sagForce(i,1);
+        urMoment(count,1) = moment_all(i,1);
+        sumForce = sumForce + sagForce(i,1);
+        sumMoment = sumMoment + moment_all(i,1);
+    end
+end
+%urStDev = std(urMomentArmPercentFoot)
+urForceMean = sumForce/count;
+urMomentMean = sumMoment/count;
+urMomentArmMean = urMomentMean / urForceMean;
+urMomentArmPercentFootMean = (urMomentArmMean / 0.24)*100
+
+%% SECTION 9B: LEVEL GROUND ANALYSIS
+count = 0;
+sumForce = 0;
+sumMoment = 0;
+for i = 1:length(moment_all)
+    if i >= lgStart1 && i < lgEnd1 + 1 || ...
+            i >= lgStart2 && i < lgEnd2 + 1 || ...
+            i >= lgStart3 && i < lgEnd3 + 1
+        count = count + 1;
+        lgForce(count,1) = sagForce(i,1);
+        lgMoment(count,1) = moment_all(i,1);
+        sumForce = sumForce + sagForce(i,1);
+        sumMoment = sumMoment + moment_all(i,1);
+    end
+end
+
+%lgStDev = std(lgMomentArmPercentFoot)
+lgForceMean = sumForce/count;
+lgMomentMean = sumMoment/count;
+lgMomentArmMean = lgMomentMean / lgForceMean;
+lgMomentArmPercentFootMean = (lgMomentArmMean / 0.24)*100
+
+%% SECTION 9C: DOWN RAMP ANALYSIS
+count = 0;
+sumForce = 0;
+sumMoment = 0;
+for i = 1:length(moment_all)
+    if i >= drStart1 && i < drEnd1 + 1 || ...
+            i >= drStart2 && i < drEnd2 + 1
+        count = count + 1;
+        drForce(count,1) = sagForce(i,1);
+        drMoment(count,1) = moment_all(i,1);
+        sumForce = sumForce + sagForce(i,1);
+        sumMoment = sumMoment + moment_all(i,1);
+    end
+end
+
+%drStDev = std(drMomentArmPercentFoot)
+drForceMean = sumForce/count;
+drMomentMean = sumMoment/count;
+drMomentArmMean = drMomentMean / drForceMean;
+drMomentArmPercentFootMean = (drMomentArmMean / 0.24)*100
+
+%% SECTION 9D: UP STAIRS ANALYSIS
+count = 0;
+sumForce = 0;
+sumMoment = 0;
+for i = 1:length(moment_all)
+    if i >= usStart1 && i < usEnd1 + 1 || ...
+            i >= usStart2 && i < usEnd2 + 1
+        count = count + 1;
+        usForce(count,1) = sagForce(i,1);
+        usMoment(count,1) = moment_all(i,1);
+        sumForce = sumForce + sagForce(i,1);
+        sumMoment = sumMoment + moment_all(i,1);
+    end
+end
+
+%usStDev = std(usMomentArmPercentFoot)
+usForceMean = sumForce/count;
+usMomentMean = sumMoment/count;
+usMomentArmMean = usMomentMean / usForceMean;
+usMomentArmPercentFootMean = (usMomentArmMean / 0.24)*100
+
+
+%% SECTION 9E: DOWN STAIRS ANALYSIS
+%something is not right
+count = 0;
+sumForce = 0;
+sumMoment = 0;
+for i = 1:length(moment_all)
+    if i >= dsStart1 && i < dsEnd1 + 1 || ...
+            i >= dsStart2 && i < dsEnd2 + 1
+        count = count + 1;
+        dsForce(count,1) = sagForce(i,1);
+        dsMoment(count,1) = moment_all(i,1);
+        sumForce = sumForce + sagForce(i,1);
+        sumMoment = sumMoment + moment_all(i,1);
+    end
+end
+
+%dsStDev = std(dsMomentArmPercentFoot)
+dsForceMean = sumForce/count;
+dsMomentMean = sumMoment/count;
+dsMomentArmMean = dsMomentMean / dsForceMean;
+dsMomentArmPercentFootMean = (dsMomentArmMean / 0.24)*100
+
+
+%% SECTION 10: GRAPH DMAMA
+
+X = categorical({'Level Ground','Up Ramp','Down Ramp','Up Stairs','Down Stairs'});
+X = reordercats(X,{'Level Ground','Up Ramp','Down Ramp','Up Stairs','Down Stairs'});
+Y = [lgMomentArmPercentFootMean urMomentArmPercentFootMean drMomentArmPercentFootMean ...
+    usMomentArmPercentFootMean dsMomentArmPercentFootMean];
+%error = [lgStDev urStDev drStDev usStDev dsStDev];
+
+
+titleV=(['DMAMA: Subject ', num2str(subject), ' on Setting ', num2str(setting)]);
+
+figure
+bar(X,Y)
+hold on
+%errorbar(Y, error, '.')
+ylim([0 30])
+title(titleV)
+ylabel('DMAMA (% of foot length)')
+text(1:length(Y),Y,num2str(Y'),'vert','bottom','horiz','center');
+hold off
+
+%% SECTION 11: EXTRA INFO GRAPHS
+%Show the moment and force graphs, bad graph because its just showing
+%stance phase, but good for seeing outliers and trends.
+
+%Force
+titleV=(['Force Plot of Each Ambulation Mode for: Subject ', num2str(subject), ' on Setting ', num2str(setting)]);
+figure
+subplot(5,1,1)
+plot(lgForce)
+xlim([0 length(lgForce)])
+title('Level Ground')
+ylabel('Force (N)')
+
+subplot(5,1,2)
+plot(urForce)
+xlim([0 length(urForce)])
+title('Up Ramp')
+ylabel('Force (N)')
+
+subplot(5,1,3)
+plot(drForce)
+title('Down Ramp')
+xlim([0 length(drForce)])
+ylabel('Force (N)')
+
+subplot(5,1,4)
+plot(usForce)
+title('Up Stairs')
+xlim([0 length(usForce)])
+ylabel('Force (N)')
+
+subplot(5,1,5)
+plot(dsForce)
+title('Down Stairs')
+xlim([0 length(dsForce)])
+ylabel('Force (N)')
+
+sgtitle(titleV)
+
+%Moment
+titleV=(['Moment Plot of Each Ambulation Mode for: Subject ', num2str(subject), ' on Setting ', num2str(setting)]);
+figure
+subplot(5,1,1)
+plot(lgMoment, 'r')
+xlim([0 length(lgForce)])
+title('Level Ground')
+ylabel('Moment (Nm)')
+
+subplot(5,1,2)
+plot(urMoment, 'r')
+xlim([0 length(urForce)])
+title('Up Ramp')
+ylabel('Moment (Nm)')
+
+subplot(5,1,3)
+plot(drMoment, 'r')
+xlim([0 length(drForce)])
+title('Down Ramp')
+ylabel('Moment (Nm)')
+
+subplot(5,1,4)
+plot(usMoment, 'r')
+xlim([0 length(usForce)])
+title('Up Stairs')
+ylabel('Moment (Nm)')
+
+subplot(5,1,5)
+plot(dsMoment, 'r')
+xlim([0 length(dsForce)])
+title('Down Stairs')
+ylabel('Moment (Nm)')
+
+sgtitle(titleV)
+
+%Both
+titleV=(['Force Plot of Each Ambulation Mode for: Subject ', num2str(subject), ' on Setting ', num2str(setting)]);
+figure
+subplot(5,1,1)
+xlim([0 length(lgForce)])
+yyaxis left
+plot(lgForce)
+ylabel('Force (N)')
+yyaxis right
+plot(lgMoment)
+ylabel('Moment (Nm)')
+title('Level Ground')
+
+subplot(5,1,2)
+xlim([0 length(urForce)])
+yyaxis left
+plot(urForce)
+ylabel('Force (N)')
+yyaxis right
+plot(urMoment)
+ylabel('Moment (Nm)')
+title('Up Ramp')
+
+subplot(5,1,3)
+xlim([0 length(drForce)])
+yyaxis left
+plot(drForce)
+ylabel('Force (N)')
+yyaxis right
+plot(drMoment)
+ylabel('Moment (Nm)')
+title('Down Ramp')
+
+subplot(5,1,4)
+xlim([0 length(usForce)])
+yyaxis left
+plot(usForce)
+ylabel('Force (N)')
+yyaxis right
+plot(usMoment)
+ylabel('Moment (Nm)')
+title('Up Stairs')
+
+subplot(5,1,5)
+xlim([0 length(dsForce)])
+yyaxis left
+plot(dsForce)
+ylabel('Force (N)')
+yyaxis right
+plot(dsMoment)
+ylabel('Moment (Nm)')
+title('Down Stairs')
+
+sgtitle(titleV)
