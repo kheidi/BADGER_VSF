b73aaa8ef8705d76e2c33f2483916c808bf4626d
Katherine Fehr <kfehr@wisc.edu>
2021-08-10 23:31:18 -0500

    Start modifying for sub11

--- Tools/vectorCalc3D_sub11.m
+++ Tools/vectorCalc3D_sub11.m
@@ -1,6 +1,6 @@
 clear
 
-load visual3d_12.mat
+load visual3d_11.mat
 shankAngle_all = shkang{1,1}(:,1:3);
 SK2_all = SK2{1,1}(:,1:3); %RS Marker 1; inline with #3, lateral marker
 SK3_all = SK3{1,1}(:,1:3); %RS Marker 2; posterior side of ipecs
@@ -16,8 +16,8 @@ vsf_ankle_all = VSF_ANKLE{1,1}(:,1:3);
 % vsf_ankle_all = VSF_LANKLE{1,1}(:,1:3);
 
 % -- Change this frame section for each subject! --
-% frames = randi([3580 3781], 1, 200); %[3580 3781] for subject 11, [8532 9032] for subject 12
-frames = [8532:8532+299];
+frames = [3425:3725]; %[3580 3781] for subject 11, [8532 9032] for subject 12
+% frames = [8532:8532+299];
 r = zeros(length(frames),3);
 shankL = zeros(10,1);
 
@@ -27,14 +27,10 @@ for i= 1: length(frames)
     shankframe_origen = [vsf_ankle_all(fN,:)];
     knee = Knee_all(fN,:);
 
-    % Need to fix
-    LiPMark = SK4_all(fN,:); % the subraction lowers the markers since they were on a plate above the iPecs
+    % For sub 11: sk2 is marker1, sk2 is marker2, sk3 is marker3
+    LiPMark = SK2_all(fN,:); % the subraction lowers the markers since they were on a plate above the iPecs
     BiPMark = SK3_all(fN,:); %[0,0,0.04872]
-    RiPMark = SK2_all(fN,:);
-    
-    LiPMark = SK4_all(fN,:); % the subraction lowers the markers since they were on a plate above the iPecs
-    BiPMark = SK2_all(fN,:); %[0,0,0.04872]
-    RiPMark = SK3_all(fN,:);
+    RiPMark = SK4_all(fN,:);
     
     % Find iPecs center
     % Midpoint b/w RSK2 and RSK4
@@ -54,15 +50,10 @@ for i= 1: length(frames)
     u_iUp = u_iUp / norm(u_iUp);
     
     v_iMid2iL = -1*v_iMid2iR;
-   
     
     Av = iPecsMid;
     Bv = RiPMark;
     Cv = BiPMark;
-    
-%     V1 = (Bv-Av)/norm(Bv-Av,2); % mid to right
-%     V2 = cross(V1,(Cv-Av))/norm(cross(V1,(Cv-Av)),2); % mid to back
-%     V3 = cross(V1,V2)/norm(cross(V1,V2)); % up
     
     V1 = (Bv-Av)/norm(Bv-Av,2); % mid to right
     V2 = cross(V1,(Cv-Av))/norm(cross(V1,(Cv-Av)),2); % mid to right cross mid to back: down
@@ -72,14 +63,9 @@ for i= 1: length(frames)
     ucheck1 = dot(V1, V2);
     ucheck2 = dot(V2, V3);
     ucheck3 = dot(V1, V3);
-   
     
-%     iX = v_iMid2iR; %X
-%     iY = u_iMid2iF; %Y
-%     iZ = u_iUp; %Z
-%     
-    iX = V3; %X
-    iY = V1; %Y
+    iX = V1; %X
+    iY = V3; %Y
     iZ = -V2; %Z
     
     
